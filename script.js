ddf8459b4598394f41961b250bc4f47492f13556
Luis Balbás <lbq200@gmail.com>
2021-05-27 19:15:59 -0400

    Finished game logic, started with animation logic

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const resultList = {
 	paper: {
 		scissors: false,
 		rock: true,
+		image: '<span><img src="images/icon-paper.svg" alt="Paper" /></span>',
 	},
 	rock: {
 		paper: false,
 		scissors: true,
+		image: '<span><img src="images/icon-rock.svg" alt="Rock" /></span>',
 	},
 	scissors: {
 		rock: false,
 		paper: true,
+		image: '<span><img src="images/icon-scissors.svg" alt="Scissors" /></span>',
 	},
 };
 
@@ -18,6 +21,7 @@ var userScore = 0;
 var pickedContainer = document.getElementById("pickedContainer");
 var movesChooseCont = document.getElementById("movesCont");
 var resultContainer = document.getElementById("resultContainer");
+
 var playerMove;
 var houseMove;
 
@@ -66,16 +70,19 @@ function finishGame(result) {
 			displayResult("YOU LOSE");
 			break;
 	}
-	updateScore();
 }
 
 function displayResult(text) {
 	hide(movesChooseCont);
 	unhide(pickedContainer);
+
 	document.getElementById("result").innerHTML = text;
 
 	showPicks();
-	delay(() => unhide(resultContainer), 5000);
+	delay(() => {
+		updateScore();
+		unhide(resultContainer);
+	}, 2500);
 }
 
 function updateScore() {
@@ -92,7 +99,27 @@ function unhide(element) {
 function delay(fnc, time) {
 	setTimeout(() => {
 		fnc();
-		console.log("Hi");
 	}, time);
 }
-function showPicks() {}
+function showPicks() {
+	delay(() => {
+		document.getElementById("playerMove").classList.add(playerMove);
+		document.getElementById("playerMove").innerHTML =
+			resultList[playerMove].image;
+	}, 500);
+	delay(() => {
+		document.getElementById("houseMove").classList.add(houseMove);
+		document.getElementById("houseMove").innerHTML =
+			resultList[houseMove].image;
+	}, 1500);
+}
+function newGame() {
+	document.getElementById("playerMove").classList.remove(playerMove);
+	document.getElementById("playerMove").innerHTML = "";
+	document.getElementById("houseMove").innerHTML = "";
+	document.getElementById("houseMove").classList.remove(houseMove);
+
+	hide(pickedContainer);
+	hide(resultContainer);
+	unhide(movesChooseCont);
+}
